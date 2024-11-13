ab3d0bc8a1c1d639429e1b89f3665fd63866010c
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-11-12 21:15:27 -0300

    Ajuste Menu

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,46 +5,42 @@ module HomeHelper
             column: "first-column",
             items: [
                 { text: "Populares", href: "https://www.crunchyroll.com/pt-br/videos/popular" },
-                { text: "Novidades", href: "#" },
-                { text: "A-Z", href: "#" },
-                { text: "Simulcasts da Temporada", href: "#" },
-                { text: "Calendário de Lançamentos", href: "#" },
-                { text: "Videoclipes & Shows", href: "#" }
+                { text: "Novidades", href: "https://www.crunchyroll.com/pt-br/videos/new" },
+                { text: "A-Z", href: "https://www.crunchyroll.com/pt-br/videos/alphabetical" },
+                { text: "Simulcasts da Temporada", href: "https://www.crunchyroll.com/pt-br/simulcasts/seasons/fall-2024" },
+                { text: "Calendário de Lançamentos", href: "https://www.crunchyroll.com/pt-br/simulcastcalendar" },
+                { text: "Videoclipes & Shows", href: "https://www.crunchyroll.com/pt-br/music" }
             ]
             },
             {
-            column: nil, # Linha divisória
-            divider: true
-            },
-            {
-            column: nil,
+            column: "genres-section",
             title: "Gêneros",
             items: [
-                { text: "Ação", href: "#" },
-                { text: "Aventura", href: "#" },
-                { text: "Comédia", href: "#" },
-                { text: "Drama", href: "#" },
-                { text: "Fantasia", href: "#" }
+                { text: "Ação", href: "https://www.crunchyroll.com/pt-br/videos/action" },
+                { text: "Música", href: "https://www.crunchyroll.com/pt-br/videos/music" },
+                { text: "Shounen", href: "https://www.crunchyroll.com/pt-br/videos/shonen" },
+                { text: "Aventura", href: "https://www.crunchyroll.com/pt-br/videos/adventure" },
+                { text: "Romance", href: "https://www.crunchyroll.com/pt-br/videos/romance" }
             ]
             },
             {
-            column: nil,
+            column: "genres-section",
             items: [
-                { text: "Música", href: "#" },
-                { text: "Romance", href: "#" },
-                { text: "Ficção Científica", href: "#" },
-                { text: "Seinen", href: "#" },
-                { text: "Shoujo", href: "#" }
+                { text: "Slice-of-Life", href: "https://www.crunchyroll.com/pt-br/videos/slice-of-life" },
+                { text: "Comédia", href: "https://www.crunchyroll.com/pt-br/videos/comedy" },
+                { text: "Ficção Científica", href: "https://www.crunchyroll.com/pt-br/videos/sci-fi" },
+                { text: "Esportes", href: "https://www.crunchyroll.com/pt-br/videos/sports" },
+                { text: "Drama", href: "https://www.crunchyroll.com/pt-br/videos/drama" }
             ]
             },
             {
-            column: nil,
+            column: "genres-section",
             items: [
-                { text: "Shounen", href: "#" },
-                { text: "Slice-of-Life", href: "#" },
-                { text: "Esportes", href: "#" },
-                { text: "Sobrenatural", href: "#" },
-                { text: "Suspense", href: "#" }
+                { text: "Seinen", href: "https://www.crunchyroll.com/pt-br/videos/seinen" },
+                { text: "Sobrenatural", href: "https://www.crunchyroll.com/pt-br/videos/supernatural" },
+                { text: "Fantasia", href: "https://www.crunchyroll.com/pt-br/videos/fantasy" },
+                { text: "Shoujo", href: "https://www.crunchyroll.com/pt-br/videos/shojo" },
+                { text: "Suspense", href: "https://www.crunchyroll.com/pt-br/videos/thriller" }
             ]
             }
         ]
