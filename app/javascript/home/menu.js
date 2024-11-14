fc1f9b45c950fd24176f92b385f7192927dbcab7
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-11-14 20:00:47 -0300

    Ajustes Menu

--- app/javascript/home/menu.js
+++ app/javascript/home/menu.js
@@ -7,3 +7,25 @@ document.querySelectorAll('.nav-item.dropdown').forEach(item => {
         item.classList.remove('show');
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const overlay = document.querySelector('.body-overlay');
+
+    document.querySelectorAll('.dropdown').forEach(item => {
+        // Mostra o fundo quando qualquer dropdown é aberto
+        item.addEventListener('show.bs.dropdown', () => {
+            overlay.classList.add('active');
+        });
+
+        // Verifica se ainda há dropdowns abertos antes de remover o fundo
+        item.addEventListener('hide.bs.dropdown', () => {
+            setTimeout(() => {
+                const anyOpenDropdown = document.querySelector('.dropdown-menu.show');
+                if (!anyOpenDropdown) {
+                    overlay.classList.remove('active');
+                }
+            }, 200); // Tempo para permitir a abertura de outro dropdown
+        });
+    });
+});
