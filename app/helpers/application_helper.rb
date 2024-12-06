21051cb9d4b4e48cc0395237e371adb032cff23d
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-12-06 20:25:06 -0300

    Ajuste footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def dropdown_menu_items_footer
+        [
+            {text: "Inglês (EUA)"},
+            {text: "Espanhol"},
+            {text: "Espanhol (Espanha)"},
+            {text: "Português (Brasil)"},
+            {text: "Português (Portugal)"},
+            {text: "Francês (França)"},
+            {text: "Alemão"},
+            {text: "Árabe"},
+            {text: "Italiano"},
+            {text: "Russo"},
+            {text: "Hindi"}
+        ]
+    end
 end
