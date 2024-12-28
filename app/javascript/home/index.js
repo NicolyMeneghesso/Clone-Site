bf7f9398e8b8a4caf95aa67bc5ee85a81cfbe363
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-12-28 16:26:13 -0300

    ajuste home

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -1,13 +1,22 @@
 document.querySelectorAll(".carousel-overlay p").forEach((el) => {
-    const maxLines = 4;
-    const lineHeight = parseFloat(window.getComputedStyle(el).lineHeight);
-    const maxHeight = maxLines * lineHeight;
-  
-    if (el.scrollHeight > maxHeight) {
-      el.style.overflow = "hidden";
-      el.style.textOverflow = "ellipsis";
-      el.style.display = "-webkit-box";
-      el.style.webkitBoxOrient = "vertical";
-      el.style.webkitLineClamp = maxLines;
-    }
+  const maxLines = 4;
+  const lineHeight = parseFloat(window.getComputedStyle(el).lineHeight);
+  const maxHeight = maxLines * lineHeight;
+
+  if (el.scrollHeight > maxHeight) {
+    el.style.overflow = "hidden";
+    el.style.textOverflow = "ellipsis";
+    el.style.display = "-webkit-box";
+    el.style.webkitBoxOrient = "vertical";
+    el.style.webkitLineClamp = maxLines;
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
 });
